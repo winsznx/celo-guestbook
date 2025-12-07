0560d1c2005d00430a1be15dc2c77f65a21e516d
winsznx <timjosh507@gmail.com>
2025-12-07 15:24:19 +0100

    feat: upgrade Farcaster SDK to miniapp-sdk and introduce isLoading state to guestbook page

--- app/context/FrameProvider.jsx
+++ app/context/FrameProvider.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { createContext, useContext, useEffect, useState } from "react";
-import sdk from "@farcaster/frame-sdk";
+import sdk from "@farcaster/miniapp-sdk";
 
 const FrameContext = createContext({
     isSDKLoaded: false,
